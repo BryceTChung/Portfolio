ee008fef2dabea5e16cbec4a22c483833e24aa1f
Chris <ctran209@outlook.com>
2021-05-23 23:49:46 -0700

    website

--- web_portfolio/src/components/Header.js
+++ web_portfolio/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import './Header.css'
 const Header = () => {
 	return (
-		<div>
+		<div class ="container">
+			<div class="header">
+			<h1>FirstName LastName</h1>
+			<div class ="subheading">
+			<h2>title of job or something</h2>
+			</div>
+			</div>
+			<div class="sovaoutside">
+			<img src="https://gamezo.co.uk/wp-content/uploads/2020/11/Sova-Guide.png" id="sova"/>
+			</div>
+			
+			
 			
 		</div>
 	)
